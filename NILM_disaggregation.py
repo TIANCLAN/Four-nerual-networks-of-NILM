f70fcdc2666011a96988091462b2a11dc5b1901f
TIANCLAN <384270260@qq.com>
2022-07-28 00:35:56 +0800

    ”nilm“

---
+++ NILM_disaggregation.py
@@ -0,0 +1,207 @@
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import tensorflow.keras.backend as K
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import datetime
+import argparse
+from VAE_functions import *
+from NILM_functions import *
+import pickle
+from scipy.stats import norm
+from keras.utils.vis_utils import plot_model
+from dtw import *
+import logging
+import json
+
+ADD_VAL_SET = False
+
+logging.getLogger('tensorflow').disabled = True
+
+###############################################################################
+# Config
+###############################################################################
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--gpu", default=0, type=int, help="Appliance to learn")
+parser.add_argument("--config", default="", type=str, help="Path to the config file")
+a = parser.parse_args()
+
+# Select GPU
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = str(a.gpu)
+
+
+print("###############################################################################")
+print("NILM DISAGREGATOR")
+print("GPU : {}".format(a.gpu))
+print("CONFIG : {}".format(a.config))
+print("###############################################################################")
+
+with open(a.config) as data_file:
+    nilm = json.load(data_file)
+print(nilm["appliance"])
+
+np.random.seed(123)
+
+name = "NILM_Disag_{}".format(nilm["appliance"])
+time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+for r in range(1, nilm["run"]+1):
+    ###############################################################################
+    # Load dataset
+    ###############################################################################
+    x_train, y_train = load_data(nilm["model"], nilm["appliance"], nilm["dataset"], nilm["preprocessing"]["width"], nilm["preprocessing"]["strides"], set_type="train")
+    
+    main_mean = nilm["preprocessing"]["main_mean"]
+    main_std = nilm["preprocessing"]["main_std"]
+
+    app_mean = nilm["preprocessing"]["app_mean"]
+    app_std = nilm["preprocessing"]["app_std"]
+
+    ###############################################################################
+    # Training parameters
+    ###############################################################################
+    epochs = nilm["training"]["epoch"]
+    batch_size = nilm["training"]["batch_size"]
+
+    STEPS_PER_EPOCH = x_train.shape[0]//batch_size
+
+    lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+                    float(nilm["training"]["lr"]), 
+                    decay_steps=STEPS_PER_EPOCH*nilm["training"]["decay_steps"],
+                    decay_rate=1,
+                    staircase=False)
+
+    ###############################################################################
+    # Optimizer
+    ###############################################################################
+    def get_optimizer(opt):
+        if opt == "adam":
+            return tf.keras.optimizers.Adam(lr_schedule)
+        else:
+            return tf.keras.optimizers.RMSprop(lr_schedule)
+
+
+    ###############################################################################
+    # Create and initialize the model
+    ###############################################################################
+    if nilm["model"] == "VAE":
+        model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer=get_optimizer(nilm["training"]["optimizer"]))
+    elif nilm["model"] == "DAE":
+        model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer="Adam")
+    elif nilm["model"] == "S2P":
+        model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer=tf.keras.optimizers.Adam(learning_rate=nilm["training"]["lr"], beta_1=0.9, beta_2=0.999))
+    elif nilm["model"] == "S2S":
+        model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer=tf.keras.optimizers.Adam(learning_rate=nilm["training"]["lr"], beta_1=0.9, beta_2=0.999))
+
+    ###############################################################################
+    # Callback checkpoint settings
+    ###############################################################################
+    
+    list_callbacks = []
+    
+    # Create a callback that saves the model's weights
+    if nilm["training"]["save_best"] == 1:
+        checkpoint_path = "../../data/NILM/{}/{}/{}/logs/model/House_{}/{}/{}".format(name, nilm["dataset"]["name"], nilm["model"], nilm["dataset"]["test"]["house"][0], time, r) +"/checkpoint.ckpt"
+        checkpoint_dir = os.path.dirname(checkpoint_path)
+        
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                         save_weights_only=True,
+                                                         verbose=0,
+                                                         monitor="val_mean_absolute_error",
+                                                         mode="min",
+                                                         save_best_only=True)
+    else:
+        checkpoint_path = "../../data/NILM/{}/{}/{}/logs/model/House_{}/{}/{}".format(name, nilm["dataset"]["name"], nilm["model"], nilm["dataset"]["test"]["house"][0], time, r) +"/cp-{epoch:04d}.ckpt"
+        checkpoint_dir = os.path.dirname(checkpoint_path)
+        
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                         save_weights_only=True,
+                                                         verbose=0,
+                                                         period=1)
+        
+    list_callbacks.append(cp_callback)
+
+    if nilm["training"]["patience"] > 0:
+        patience = nilm["training"]["patience"]
+        start_epoch = nilm["training"]["start_stopping"]
+        
+        print("Patience : {}, Start at : {}".format(patience, start_epoch))
+        
+        es_callback = CustomStopper(monitor='val_loss', patience=patience, start_epoch=start_epoch, mode="auto")
+        
+        list_callbacks.append(es_callback)
+    
+    ###############################################################################
+    # Normalize Test Data and History Callback
+    ###############################################################################
+    if ADD_VAL_SET:
+        if nilm["dataset"]["name"] == "ukdale":
+            if nilm["model"] == "S2P":
+                x_test_s2p, y_test_s2p = transform_s2p(x_test, y_test, nilm["preprocessing"]["width"], nilm["training"]["S2P_strides"])
+                history_cb = AdditionalValidationSets([((x_test_s2p-main_mean)/main_std, (y_test_s2p-app_mean)/app_std, 'House_2')], verbose=1)
+            else:
+                history_cb = AdditionalValidationSets([((x_test-main_mean)/main_std, (y_test-app_mean)/app_std, 'House_2')], verbose=1)
+
+        elif nilm["dataset"]["name"] == "house_2":
+            history_cb = AdditionalValidationSets([(x_test, y_test, 'House_2')], verbose=1)
+        elif nilm["dataset"]["name"] == "refit":
+            history_cb = AdditionalValidationSets([(x_test, y_test, 'House_2')], verbose=1)
+
+        list_callbacks.append(history_cb)
+    
+    ###############################################################################
+    # Summary of all parameters
+    ###############################################################################
+    print("###############################################################################")
+    print("Summary")
+    print("###############################################################################")
+    print("{}".format(nilm))
+    print("Run number : {}/{}".format(r,nilm["run"]))
+    print("###############################################################################")
+
+    if not os.path.exists("../../data/NILM/{}/{}/{}/logs/model/House_{}/{}".format(name, nilm["dataset"]["name"], nilm["model"], nilm["dataset"]["test"]["house"][0], time)):
+        os.makedirs("../../data/NILM/{}/{}/{}/logs/model/House_{}/{}".format(name, nilm["dataset"]["name"], nilm["model"], nilm["dataset"]["test"]["house"][0], time))
+
+    with open("../../data/NILM/{}/{}/{}/logs/model/House_{}/{}/config.txt".format(name, nilm["dataset"]["name"], nilm["model"], nilm["dataset"]["test"]["house"][0], time), "w") as outfile:
+        json.dump(nilm, outfile)
+
+    ###############################################################################
+    # Train Model
+    ###############################################################################
+    if nilm["dataset"]["name"] == "ukdale":
+        ###############################################################################
+        # Real Validation
+        ###############################################################################
+        if nilm["model"] == "S2P":
+            x_train_s2p, y_train_s2p = transform_s2p(x_train, y_train, nilm["preprocessing"]["width"], nilm["training"]["S2P_strides"])
+            
+            history = model.fit((x_train_s2p-main_mean)/main_std, (y_train_s2p-app_mean)/app_std, validation_split=nilm["training"]["validation_split"], shuffle=True, 
+                                epochs=epochs, batch_size=batch_size, callbacks=list_callbacks, verbose=1, initial_epoch=0)
+
+        elif nilm["model"] == "VAE":    
+            history = model.fit((x_train-main_mean)/main_std, (y_train-app_mean)/app_std, validation_split=nilm["training"]["validation_split"], shuffle=True, 
+                                epochs=epochs, batch_size=batch_size, callbacks=list_callbacks, verbose=1, initial_epoch=0)
+            
+        elif nilm["model"] == "S2S":    
+            history = model.fit((x_train-main_mean)/main_std, (y_train-app_mean)/app_std, validation_split=nilm["training"]["validation_split"], shuffle=True, 
+                                epochs=epochs, batch_size=batch_size, callbacks=list_callbacks, verbose=1, initial_epoch=0)
+            
+        elif nilm["model"] == "DAE":    
+            history = model.fit((x_train-main_mean)/main_std, (y_train-app_mean)/app_std, validation_split=nilm["training"]["validation_split"], shuffle=True, 
+                                epochs=epochs, batch_size=batch_size, callbacks=list_callbacks, verbose=1, initial_epoch=0)
+
+        ###############################################################################
+        # Save history
+        ###############################################################################
+        np.save("../../data/NILM/{}/{}/{}/logs/model/House_{}/{}/{}/history.npy".format(name, nilm["dataset"]["name"], nilm["model"], nilm["dataset"]["test"]["house"][0], time, r), history.history)
+        #np.save("{}/{}/{}/logs/model/{}/{}/history_cb_{}.npy".format(name, nilm["dataset"]["name"], nilm["model"], time, r, epochs), history_cb.history)
+
+        print("Fit finished!")
+    else:
+        print("Error in dataset name!")
+
---
+++ NILM_test.py
@@ -0,0 +1,284 @@
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import tensorflow.keras.backend as K
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import datetime
+import argparse
+from VAE_functions import *
+from NILM_functions import *
+import pickle
+from scipy.stats import norm
+from keras.utils.vis_utils import plot_model
+from dtw import *
+import logging
+import json
+
+logging.getLogger('tensorflow').disabled = True
+
+###############################################################################
+# Config
+###############################################################################
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--gpu", default=0, type=int, help="GPU to use")
+parser.add_argument("--config", default="", type=str, help="Path to the config file")
+parser.add_argument("--time", default="", type=str, help="Folder name containing runs")
+parser.add_argument("--save_pred", default=True, type=bool, help="Save y_pred_all")
+parser.add_argument("--previous", default=1, type=int, help="Select previous experiment")
+a = parser.parse_args()
+
+# Select GPU
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = str(a.gpu)
+
+thr_house_2 = { "fridge" : 70,
+               "television":50,
+               "FridgeFreezer":70,
+                "WashingMachine" : 20,
+                "DishWasher" : 100,
+                "Kettle" : 100,
+                "Microwave" : 200,
+               "Toaster" :100,
+               "ActiveSpeaker":6,
+               "light":50,
+                 "fan" : 5,
+                "breadmaker" :100,
+               "CoffeeMaker":800,
+               "ElectricOven":2000,
+               "HairDryer":500,
+                  "computerMonitor" : 60}
+
+with open(a.config) as data_file:
+    nilm = json.load(data_file)
+
+#print(nilm)
+    
+np.random.seed(123)
+
+name = "NILM_Disag_{}".format(nilm["appliance"])
+previous = a.previous
+
+if a.time == "":
+    files_and_directories = os.listdir("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0]))
+    time = np.sort(files_and_directories)[-previous]
+else:
+    time = a.time
+
+# Get exact configuration for this experiment
+with open("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/config.txt".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time)) as data_file:
+    nilm = json.load(data_file)
+    print(nilm)
+    
+epochs = nilm["training"]["epoch"]
+start = nilm["training"]["start_stopping"]
+    
+print("###############################################################################")
+print("NILM DISAGREGATOR")
+print("GPU : {}".format(a.gpu))
+print("CONFIG : {}".format(a.config))
+print("FOLDER : {}".format(time))
+print("###############################################################################")
+
+###############################################################################
+# Load history files
+###############################################################################
+hist = []
+
+
+for r in range(1, nilm["run"]+1):
+    #hist.append(np.load("{}/ukdale/{}/logs/model/{}/{}/history_cb_{}.npy".format(name, nilm["model"], time, r, epochs), allow_pickle=True))
+    try:
+        hist.append(np.load("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/{}/history.npy".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time, r), allow_pickle=True))
+    except:
+        hist.append(np.load("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/{}/history_{}.npy".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time, r, epochs), allow_pickle=True))
+        
+MAE_run = []
+for r in range(len(hist)):
+    pos_val_min = np.argmin(hist[r].all()["val_mean_absolute_error"][start:epochs])
+    MAE_run.append(hist[r].all()["val_mean_absolute_error"][pos_val_min+start])
+
+print("Result : {} ± {}".format(np.mean(MAE_run), np.std(MAE_run)))
+
+
+###############################################################################
+# Create Model
+###############################################################################
+
+###############################################################################
+# Optimizer
+###############################################################################
+def get_optimizer(opt):
+    if opt == "adam":
+        return tf.keras.optimizers.Adam(0.001)
+    else:
+        return tf.keras.optimizers.RMSprop(0.001)
+
+if nilm["model"] == "VAE":
+    model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer=get_optimizer(nilm["training"]["optimizer"]))
+elif nilm["model"] == "DAE":
+    model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer="adam")
+elif nilm["model"] == "S2P":
+    model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer=tf.keras.optimizers.Adam(learning_rate=nilm["training"]["lr"], beta_1=0.9, beta_2=0.999))
+elif nilm["model"] == "S2S":
+    model = create_model(nilm["model"], nilm["config"], nilm["preprocessing"]["width"], optimizer=tf.keras.optimizers.Adam(learning_rate=nilm["training"]["lr"], beta_1=0.9, beta_2=0.999))
+        
+app_list = nilm["appliance"]
+width = nilm["preprocessing"]["width"]
+stride = nilm["preprocessing"]["strides"]
+
+main_mean = nilm["preprocessing"]["main_mean"]
+main_std = nilm["preprocessing"]["main_std"]
+app_mean = nilm["preprocessing"]["app_mean"]
+app_std = nilm["preprocessing"]["app_std"]
+
+def transform_s2p(x, y, width=199):
+    x_s2p, y_s2p = [], []
+
+    for i in range(x.shape[0]):
+        for t in range(width):
+            x_s2p.append(x[i,t:t+width,0])
+            y_s2p.append(y[i,t+width//2+1,0])
+
+    x_s2p = np.array(x_s2p)
+    y_s2p = np.array(y_s2p)
+
+    print("Shape before S2P data transformations : {}, {}".format(x.shape, y.shape))
+    print("Shape after S2P data transformations : {}, {}".format(x_s2p.shape, y_s2p.shape))
+
+    return x_s2p, y_s2p
+
+
+MAE_run = []
+ACC_run =[]
+PR_run = []
+RE_run = []
+F1_run = []
+SAE_run = []
+FEE_run = []
+
+#Load Data
+if nilm["dataset"]["test"]["ratio"][0] < 0.1:
+    nilm["dataset"]["test"]["ratio"] = [1]
+
+if "batch" in nilm["dataset"]["test"]:
+    batch_int = np.arange(0, nilm["dataset"]["test"]["ratio"][0], nilm["dataset"]["test"]["batch"][0])
+else:
+    nilm["dataset"]["test"]["batch"] = nilm["dataset"]["test"]["ratio"]
+    batch_int = np.arange(0, nilm["dataset"]["test"]["ratio"][0], nilm["dataset"]["test"]["batch"][0])
+print(batch_int)
+
+for r in range(1, nilm["run"]+1):
+    
+    x_total = []
+    y_total_pred = []
+    y_total_true = []
+    
+    for test_from in batch_int:
+        x_test, y_test = load_data(nilm["model"], nilm["appliance"], nilm["dataset"], nilm["preprocessing"]["width"], nilm["preprocessing"]["strides"], test_from=test_from, set_type="test")
+        print(x_test.shape, y_test.shape)
+        pos_val_min = np.argmin(hist[r-1].all()["val_mean_absolute_error"][start:epochs]) + start
+        if nilm["training"]["save_best"]:
+            model.load_weights("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/{}/checkpoint.ckpt".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time, r))
+        else:
+            model.load_weights("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/{}/cp-{epoch:04d}.ckpt".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time, r, epoch=pos_val_min+1))
+        
+        if nilm["model"] == "S2P":
+            x_test_s2p, y_test_s2p = transform_s2p(x_test, y_test, width)
+            
+            y_pred = model.predict([(x_test_s2p-main_mean)/main_std], verbose=1)           
+            y_all_pred = y_pred.reshape([-1])*app_std+app_mean
+            x_all = x_test_s2p[:,(width+1)//2].reshape([-1])
+            y_all_true = y_test_s2p.reshape([-1])
+            del x_test_s2p
+            del y_test_s2p
+        elif nilm["model"] == "VAE":
+            y_pred = model.predict([(x_test-main_mean)/main_std], verbose=1)
+
+            y_all_pred = reconstruct(y_pred[:]*app_std+app_mean, width, stride, "median")
+            x_all = reconstruct(x_test[:], width, stride, "median")
+            y_all_true = reconstruct(y_test[:], width, stride, "median")
+
+        elif nilm["model"] == "S2S":
+            y_pred = model.predict([(x_test-main_mean)/main_std], verbose=1)
+
+            y_all_pred = reconstruct(y_pred[:]*app_std+app_mean, width, stride)
+            x_all = reconstruct(x_test[:], width, stride)
+            y_all_true = reconstruct(y_test[:], width, stride)
+
+        elif nilm["model"] == "DAE":
+            y_pred = model.predict([(x_test-main_mean)/main_std], verbose=1)
+
+            y_all_pred = reconstruct(y_pred[:]*app_std+app_mean, width, stride)
+            x_all = reconstruct(x_test[:], width, stride)
+            y_all_true = reconstruct(y_test[:], width, stride)
+
+        #y_all_true[y_all_true<15] = 0
+        y_all_pred[y_all_pred<15] = 0
+
+        #print(x_all.shape, y_all_pred.shape, y_all_true.shape)
+        
+        x_all = x_all.reshape([1,-1])
+        y_all_pred = y_all_pred.reshape([1,-1])
+        y_all_true = y_all_true.reshape([1,-1])
+
+        ###############################################################################
+        # Completed sequence
+        ###############################################################################
+        for i in range(x_all.shape[-1]):
+            x_total.append(x_all[0,i])
+        for i in range(y_all_pred.shape[-1]):    
+            y_total_pred.append(y_all_pred[0,i])
+        for i in range(y_all_true.shape[-1]):
+            y_total_true.append(y_all_true[0,i])
+
+        #print(len(x_total))
+        
+        del x_all
+        del y_all_pred
+        del y_all_true
+        
+    ###############################################################################
+    # Transform in array
+    ###############################################################################
+    x_total = np.array(x_total).reshape([1,-1])
+    y_total_pred = np.array(y_total_pred).reshape([1,-1])
+    y_total_true = np.array(y_total_true).reshape([1,-1])
+    
+    if a.save_pred:
+        np.save("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/pred_{}.npy".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time, r), [x_total, y_total_pred, y_total_true])
+
+    print("Best Epoch : {}".format(pos_val_min+1))
+
+    MAE_tot, MAE_app, MAE = MAE_metric(y_total_pred, y_total_true, disaggregation=True, only_power_on=False)
+    acc_P_tot, acc_P_app, acc_P = acc_Power(y_total_pred, y_total_true, disaggregation=True)
+    PR_app = PR_metric(y_total_pred, y_total_true, thr=thr_house_2[nilm["appliance"]])
+    RE_app = RE_metric(y_total_pred, y_total_true, thr=thr_house_2[nilm["appliance"]])
+    F1_app = F1_metric(y_total_pred, y_total_true, thr=thr_house_2[nilm["appliance"]])
+    SAE_app = SAE_metric(y_total_pred, y_total_true)
+    FEE_app = FEE_metric(y_total_pred, y_total_true)
+    
+    print("-------------------------------------test------------",PR_app)
+    if (np.isnan(acc_P_tot)) or (F1_app[0] == 0):
+        print("Error Detected")
+    else:
+        MAE_run.append(MAE_tot)
+        ACC_run.append(acc_P_tot)
+        PR_run.append(PR_app[0])
+        RE_run.append(RE_app[0])
+        F1_run.append(F1_app[0])
+        SAE_run.append(SAE_app[0])
+        FEE_run.append(FEE_app)
+
+print(np.mean(MAE_run), np.std(MAE_run))
+print(np.mean(ACC_run), np.std(ACC_run))
+print(np.mean(PR_run), np.std(PR_run))
+print(np.mean(RE_run), np.std(RE_run))
+print(np.mean(F1_run), np.std(F1_run))
+print(np.mean(SAE_run), np.std(SAE_run))
+print(np.mean(FEE_run), np.std(FEE_run))
+
+np.save("../../data/NILM/{}/ukdale/{}/logs/model/House_{}/{}/results_median.npy".format(name, nilm["model"], nilm["dataset"]["test"]["house"][0], time), [MAE_run, ACC_run, PR_run, RE_run, F1_run, SAE_run, FEE_run])
